dd154ed7273c287b88a016f57f69ac9caaddec39
venkatesh <venkateshpoosarla@gmail.com>
2021-08-11 01:08:02 -0700

    Time: 511 ms (11.14%), Space: 39.7 MB (66.32%) - LeetHub

---
+++ jump-game/jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean dp[]=new boolean[nums.length];
+        dp[nums.length-1]=true;
+        for(int i=nums.length-2;i>=0;i--){
+           for(int j=0;j<=nums[i] && i+j<nums.length;j++){
+               if(dp[i+j]){
+                   dp[i]=true;
+               }
+           }
+        }
+        return dp[0];
+    }
+}
