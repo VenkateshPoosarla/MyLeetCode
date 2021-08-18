4e90f43b5a976650368e64ceb7ed5005834dbd67
venkatesh <venkateshpoosarla@gmail.com>
2021-08-18 11:02:04 -0700

    Time: 1 ms (98.75%), Space: 41 MB (87.75%) - LeetHub

--- insert-interval/insert-interval.java
+++ insert-interval/insert-interval.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] ni) {
-        List<int[]> li=new ArrayList<>();
-        boolean added=false;
-        for(int []x:intervals){
-             if(x[1]<ni[0]){
-                        li.add(x);
-                    }
-            else if(x[0]>ni[1]){
-                if(!added){
+        List<int[]> li = new ArrayList<>();
+        boolean merged = false;
+        for (int[] x : intervals) {
+          // intervals before we see intersection
+            if (x[1] < ni[0]) {
+                li.add(x);
+            } // intervals after we see intersection
+            else if (x[0] > ni[1]) {
+                if (!merged) {  //check if the interval is not merged and add it to the list
                     li.add(ni);
-                    added=true;
+                    merged = true;
                 }
-                li.add(x);    
-            }else{
-                ni[0]=Math.min(ni[0],x[0]);
-                ni[1]=Math.max(ni[1],x[1]);
-                
+                li.add(x);
+            } else {  // intervals with intersection
+                ni[0] = Math.min(ni[0], x[0]);
+                ni[1] = Math.max(ni[1], x[1]);
             }
         }
-        if(!added){
-                    li.add(ni);
-                    added=true;
-                }
-        int y[][]=new int[li.size()][2];
-        for(int i=0;i<li.size();i++){
-            y[i]=li.get(i);
+        if (!merged) {  //check if the interval is not merged and add it to the list
+            li.add(ni);
+        }
+        int y[][] = new int[li.size()][2];
+        for (int i = 0; i < li.size(); i++) {
+            y[i] = li.get(i);
         }
         return y;
-        
     }
 }
