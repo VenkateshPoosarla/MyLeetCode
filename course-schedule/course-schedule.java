2a63af5742a068f6e5ab87908859712bca5dc852
venkatesh <venkateshpoosarla@gmail.com>
2021-08-18 10:58:04 -0700

    Time: 2 ms (99.08%), Space: 39.4 MB (84.17%) - LeetHub

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -1,34 +1,36 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] pre) {
-        ArrayList<Integer> []al=new ArrayList[numCourses];
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer> al[]=new ArrayList[numCourses];
         for(int i=0;i<numCourses;i++){
             al[i]=new ArrayList<>();
         }
-        for(int i=0;i<pre.length;i++){
-           al[pre[i][0]].add(pre[i][1]);
+        for(int i=0;i<prerequisites.length;i++){
+            int x[]=prerequisites[i];
+            al[x[1]].add(x[0]);
         }
+        boolean hascycle[]=new boolean[1];
         int visited[]=new int[numCourses];
-        int x[]=new int[1];
-        for(int i=0;i<visited.length;i++)
-        {
-            if(visited[i]==0){
-                dfs(al,i,visited,x);
-                if(x[0]==1) 
-                    return false;
+        for(int i=0;i<al.length;i++){
+            if(visited[i]!=2){
+                dfs(visited,al,i,hascycle);
             }
+            if(hascycle[0])
+                return false;
         }
-        return x[0]!=1;
+            return true;
     }
-    void dfs(ArrayList<Integer> []al,int idx,int visited[],int x[]){
+    
+    void dfs(int visited[],ArrayList<Integer> al[],int idx,boolean hascycle[]){
         if(visited[idx]==2)
             return;
-        if(visited[idx]==1) {
-            x[0]=1;
+        if(visited[idx]==1)
+        {
+            hascycle[0]=true;
             return;
         }
         visited[idx]=1;
-        for(int id:al[idx]){
-          dfs(al,id,visited,x); 
+        for(int x:al[idx]){
+            dfs(visited,al,x,hascycle);
         }
         visited[idx]=2;
     }
