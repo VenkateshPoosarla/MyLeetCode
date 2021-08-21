b9fb40e6db0b0ebe484b1a88e7247cdde9518a34
venkatesh <venkateshpoosarla@gmail.com>
2021-08-21 12:46:49 -0700

    Time: 142 ms (66.60%), Space: 49.8 MB (68.43%) - LeetHub

--- count-good-meals/count-good-meals.java
+++ count-good-meals/count-good-meals.java
@@ -2,7 +2,7 @@ class Solution {
     public int countPairs(int[] deliciousness) {
         int x[]=new int[22];
         calculate(x);
-        long total=0;
+        int total=0;
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<deliciousness.length;i++){
             int count=0;
@@ -10,15 +10,19 @@ class Solution {
                 count+=map.getOrDefault(x[j]-deliciousness[i],0);
              }
             map.put(deliciousness[i],map.getOrDefault(deliciousness[i],0)+1);
+            // System.out.println(total);
             total+=count;
+            total=total%1_000_000_007;
         }
-        return (int)(total%1_000_000_007);
+       
+        return total;
     }
         void calculate(int []x){
             x[0]=1;
             for(int i=1;i<x.length;i++){
                 x[i]=2*x[i-1];
             }
+            // System.out.println(Arrays.toString(x));
         }
     
 }
