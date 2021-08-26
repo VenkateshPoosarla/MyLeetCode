fc273926f35788f2a91db4a5078d718cb5e93157
venkatesh <venkateshpoosarla@gmail.com>
2021-08-26 12:22:19 -0700

    Time: 10 ms (14.21%), Space: 42.3 MB (5.50%) - LeetHub

--- meeting-rooms/meeting-rooms.java
+++ meeting-rooms/meeting-rooms.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
       Arrays.sort(intervals,(a,b)->(a[1]-b[1]));
         if(intervals.length<2) return true;
-        int start[]=intervals[0];
+        int prev[]=intervals[0];
       for(int i=1;i<intervals.length;i++)  {
-          if(start[1]>intervals[i][0])
+          if(prev[1]>intervals[i][0])
               return false;
-           start=intervals[i];
+           prev=intervals[i];
       }
         return true;
     }
