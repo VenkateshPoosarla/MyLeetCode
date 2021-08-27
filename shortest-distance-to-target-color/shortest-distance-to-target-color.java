5ad73dbc159b8f03c6175af42b1bee2b79328411
venkatesh <venkateshpoosarla@gmail.com>
2021-08-26 23:00:46 -0700

    Time: 14 ms (87.23%), Space: 75.7 MB (16.42%) - LeetHub

--- shortest-distance-to-target-color/shortest-distance-to-target-color.java
+++ shortest-distance-to-target-color/shortest-distance-to-target-color.java
@@ -1,47 +1,37 @@
 class Solution {
     public List<Integer> shortestDistanceColor(int[] colors, int[][] queries) {
         // initialization
-        List<Integer> queryResults = new ArrayList<>();
-        Map<Integer, List<Integer>> hashmap = new HashMap<>();
-
-        for (int i = 0; i < colors.length; i++) {
-            hashmap.putIfAbsent(colors[i], new ArrayList<Integer>());
-            hashmap.get(colors[i]).add(i);
+        int nC[][]=new int[3][colors.length];
+        for(int j=0;j<3;j++){
+            for(int i=0;i<colors.length;i++){
+                if(colors[i]==j+1){
+                   nC[j][i]=i; 
+                }else{
+                    if(i-1>=0)
+                        nC[j][i]=nC[j][i-1];
+                    else{
+                        nC[j][i]=colors.length+1;
+                    }
+                }
+            }
         }
-
-        // for each query, apply binary search
-        for (int i = 0; i < queries.length; i++) {
-            int target = queries[i][0], color = queries[i][1];
-            if (!hashmap.containsKey(color)) {
-                queryResults.add(-1);
-                continue;
-            }
-
-            List<Integer> indexList = hashmap.get(color);
-            int insert = Collections.binarySearch(indexList, target);
-
-            // due to its nature, we need to convert the returning values
-            // from Collections.binarySearch
-            // more details: https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/Collections.html#binarySearch(java.util.List,T)
-            if (insert < 0) {
-                insert = (insert + 1) * -1;
-            }
-
-            // Handling cases when:
-            // - the target index is smaller than all elements in the indexList
-            // - the target index is larger than all elements in the indexList
-            // - the target index sits between the left and right boundaries
-            if (insert == 0) {
-                queryResults.add(indexList.get(insert) - target);
-            } else if (insert == indexList.size()) {
-                queryResults.add(target - indexList.get(insert - 1));
-            } else {
-                int leftNearest = target - indexList.get(insert - 1);
-                int rightNearest = indexList.get(insert) - target;
-                queryResults.add(Math.min(leftNearest, rightNearest));
-            }
-
+         for(int j=0;j<3;j++){
+              for(int i=colors.length-2;i>=0;i--){
+                  
+                  if(Math.abs(nC[j][i]-i)>Math.abs(nC[j][i+1]-i)){
+                     nC[j][i]=nC[j][i+1];  
+                  }
+              }
+         }
+        List<Integer> li=new ArrayList<>();
+        for(int []query:queries){
+            int idx=query[0];
+            int color=query[1];
+            if(nC[color-1][idx]<colors.length)
+                li.add(Math.abs(nC[color-1][idx]-idx));
+            else
+                li.add(-1);
         }
-        return queryResults;
+        return li;
     }
 }
