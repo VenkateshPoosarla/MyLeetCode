84d1c67a8a552614a6672c5d312dfdb84cd47a35
venkatesh <venkateshpoosarla@gmail.com>
2021-09-02 21:14:07 -0700

    Time: 4 ms (91.09%), Space: 47.6 MB (16.71%) - LeetHub

--- divide-chocolate/divide-chocolate.java
+++ divide-chocolate/divide-chocolate.java
@@ -1,17 +1,17 @@
 class Solution {
    
     public int maximizeSweetness(int[] sweetness, int K) {
-        int left = 1, right = (int)1e9 / (K + 1);
+        int sum=1000_000_000;///Arrays.stream(sweetness).sum()/(K+1);
+        int left = 1, right = sum;
         while (left < right) {
             int mid = (left + right + 1) / 2;
-            
             int cuts=makeCutsWithMidOrHigh(sweetness,K,mid);
             if (cuts > K)
                 left = mid;
             else
                 right = mid - 1;
         }
-        return left;
+        return right;
     }
     int makeCutsWithMidOrHigh(int[] sweetness,int K,int mid){
         int cur = 0, cuts = 0;
