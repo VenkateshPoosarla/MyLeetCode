7d24117f2e87076f6b734a6a2f09f812103709ef
venkatesh <venkateshpoosarla@gmail.com>
2021-09-06 23:03:50 -0700

    Time: 5 ms (56.99%), Space: 35.4 MB (97.44%) - LeetHub

--- consecutive-numbers-sum/consecutive-numbers-sum.java
+++ consecutive-numbers-sum/consecutive-numbers-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int consecutiveNumbersSum(int N) {
+        // N = k * i + (i - 1) * i / 2 => N - (i - 1) * i / 2 = k * i
        int ans = 0;
         for (int i = 1; i * (i - 1) / 2 < N; ++i)
             if ((N - i * (i - 1) / 2) % i == 0)
