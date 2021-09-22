7755995493cc5feab253ee7257fef1e3f56c140b
venkatesh <venkateshpoosarla@gmail.com>
2021-09-21 19:46:33 -0700

    Time: 0 ms (100.00%), Space: 36.3 MB (70.03%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -5,15 +5,6 @@ class Solution {
     public int simpleRobber(int[] nums,int start,int end){
         int h1=0,h2=0;
         int sum=0;
-        // if(nums.length<3){
-        // if(nums.length==1){
-        //     return nums[0];
-        // }else{
-        //     return Math.max(nums[0],nums[1]);
-        // }
-        // }
-        // h1=nums[start];
-        // h2=nums[start+1];
         for(int i=start;i<end;i++){
             int temp=Math.max(h1+nums[i],h2);
             h1=h2;
